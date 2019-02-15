018fb3b0c1d9086a5c4e1d11e76d4387ddec0daf
Ge Tan <tange116@gmail.com>
2019-02-14 19:55:38 -0500

    Add high five

--- 0_Template.py
+++ 0_Template.py
@@ -1,5 +1,6 @@
 # # # # -*- coding: utf-8 -*-
 
+import heapq
 
 if __name__ == '__main__':
     s = Solution()
--- highestFive.py
+++ highestFive.py
@@ -3,6 +3,8 @@
 Given results = [[1,91],[1,92],[2,93],[2,99],[2,98],[2,97],[1,60],[1,58],[2,100],[1,61]]
 每人至少有5份成绩，要取最高的5份的average
 """
+
+# Solution 1
 def highestFive(array):
     result = {}
     for s in array:
@@ -20,6 +22,40 @@ def highestFive(array):
     print(result)
     return result
 
+# Solution 2
+class Solution:
+    # @param {Record[]} results a list of <student_id, score>
+    # @return {dict(id, average)} find the average of 5 highest scores for each person
+    # <key, value> (student_id, average_score)
+    def getAvg(self, scores):
+        return sum(scores) / 5.0
+
+    def highFive(self, results):
+
+        # process raw data
+        record = {}
+        for r in results:
+            # student = r.id
+            # score = r.score
+            student = r[0]
+            score = r[1]
+            if student not in record:
+                record[student] = []
+            record[student].append(score)
+
+        # if student has more than 5 scores, get top 5
+        ans = {}
+        for stu in record:
+            if len(record[stu]) >= 5:
+                record[stu] = sorted(record[stu], reverse = True)[:5]
+                avg = self.getAvg(record[stu])
+                ans.update({stu: avg})
+
+        return ans
+
 if __name__ == '__main__':
     array = [[1,91],[1,92],[2,93],[2,99],[2,98],[2,97],[1,60],[1,58],[2,100],[1,61],[1,100]]
     highestFive(array)
+
+    s = Solution()
+    print(s.highFive(array))
