a27287e6b4bce197402b5ed416cd87ee0ba36d32
Ge Tan <tange116@gmail.com>
2019-03-21 22:55:44 -0400

    Add LC codes

---
+++ SlidingWindow/159_Longest_Substring_with_At_Most_Two_Distinct_Characters.py
@@ -0,0 +1,12 @@
+# # # # -*- coding: utf-8 -*-
+class Solution(object):
+    def lengthOfLongestSubstringTwoDistinct(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+
+
+if __name__ == '__main__':
+    s = Solution()
+    
