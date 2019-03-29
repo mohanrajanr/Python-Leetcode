169b68005e730289d247c6f7a27eff9bd618aabc
Ge Tan <tange116@gmail.com>
2019-03-28 22:07:29 -0400

    Add LC codes

--- 60_Permutation_Sequence.py
+++ 60_Permutation_Sequence.py
@@ -6,37 +6,29 @@ class Solution(object):
         :type k: int
         :rtype: str
         """
-        result = self.permutenums(n)
-        return result[k-1]
-        #return self.permutenums(n,k)
-
-    def permutenums(self, n):
-        nums = [i+1 for i in range(n)]
-        return self.permute(nums)
-
-    def permute(self, nums):
-        if not nums:
-            return []
-        if len(nums) == 1:
-            return [str(nums[0])]
-
-        count = 1
-        result = []
-        for i in range(0,len(nums)):
-            for item in self.permute(nums[0:i]+nums[i+1:]):
-                result.append(str(nums[i])+item)
-
+        fact = [1] * n
+        for i in range(1, n):
+            fact[i] = fact[i-1] * i
+        print (fact)
+        num = [str(i) for i in range(1,10)]
+        result = ""
+        k -= 1
+        for i in range(n, 0, -1):
+            j = k // fact[i-1]
+            k = k % fact[i-1]
+            result += num[j]
+            num.pop(j)
         return result
 
 if __name__ == '__main__':
     s = Solution()
-    n = 3
+    n = 4
     # nums = [i+1 for i in range(n)]
     # print nums
     #s.permute(n)
     # l1 = [1,2]
     # l2 = [3,4]
     # print l1+l2
-    k = 2
+    k = 17
     #print s.permutenums(n)
-    print s.getPermutation(n,k)
+    print (s.getPermutation(n,k))
